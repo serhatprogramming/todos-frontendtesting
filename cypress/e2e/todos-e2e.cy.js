a02949dc045c7b73fd1d64fc90663988009a964a
serhatprogramming <serhatprogramming@gmail.com>
2023-08-24 22:51:15 -0400

    works with testing db in the backend.

--- cypress/e2e/todos-e2e.cy.js
+++ cypress/e2e/todos-e2e.cy.js
@@ -1,42 +1,61 @@
+// describe("Todos App E2E", () => {
+//   beforeEach(() => {
+//     cy.visit("http://localhost:3000");
+//   });
+
+//   it("should open the application and display Todo Application", () => {
+//     cy.contains("Todo Application");
+//   });
+
+//   it("should display Todo List", () => {
+//     cy.contains("Todo List");
+//   });
+
+//   it("should be able to login successfully", () => {
+//     cy.get('input[type="text"]').type("johnd");
+//     cy.get('input[type="password"]').type("1234");
+//     cy.get("form").submit();
+//     cy.contains("Howdy, johnd!");
+//   });
+
+//   it("should display an error message for invalid login", () => {
+//     cy.get('input[type="text"]').type("invalidUsername");
+//     cy.get('input[type="password"]').type("invalidPassword");
+//     cy.get("form").submit();
+//     cy.contains("Invalid login credentials");
+//   });
+// });
+
+// describe("Logged-In Tests", () => {
+//   beforeEach(() => {
+//     cy.visit("http://localhost:3000");
+//     cy.get('input[type="text"]').type("johnd");
+//     cy.get('input[type="password"]').type("1234");
+//     cy.get("form").submit();
+//   });
+//   it("should add a new todo after successful login", () => {
+//     const todoText = "Test Todo Item with data-testid";
+//     cy.get('[data-testid="new-task-input"]').type(todoText);
+//     cy.contains("Add").click();
+//     cy.contains(todoText);
+//   });
+// });
+
 describe("Todos App E2E", () => {
   beforeEach(() => {
+    cy.request("POST", "http://localhost:3001/api/e2e/reset-test-db");
+    const user = {
+      name: "John Doe",
+      username: "johnd",
+      password: "1234",
+    };
+    cy.request("POST", "http://localhost:3001/api/users/", user);
     cy.visit("http://localhost:3000");
+    cy.get('input[type="text"]').type("johnd");
+    cy.get('input[type="password"]').type("1234");
+    cy.get("form").submit();
   });
-
   it("should open the application and display Todo Application", () => {
     cy.contains("Todo Application");
-  });
-
-  it("should display Todo List", () => {
-    cy.contains("Todo List");
-  });
-
-  it("should be able to login successfully", () => {
-    cy.get('input[type="text"]').type("johnd");
-    cy.get('input[type="password"]').type("1234");
-    cy.get("form").submit();
-    cy.contains("Howdy, johnd!");
-  });
-
-  it("should display an error message for invalid login", () => {
-    cy.get('input[type="text"]').type("invalidUsername");
-    cy.get('input[type="password"]').type("invalidPassword");
-    cy.get("form").submit();
-    cy.contains("Invalid login credentials");
-  });
-});
-
-describe("Logged-In Tests", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-    cy.get('input[type="text"]').type("johnd");
-    cy.get('input[type="password"]').type("1234");
-    cy.get("form").submit();
-  });
-  it("should add a new todo after successful login", () => {
-    const todoText = "Test Todo Item with data-testid";
-    cy.get('[data-testid="new-task-input"]').type(todoText);
-    cy.contains("Add").click();
-    cy.contains(todoText);
   });
 });
